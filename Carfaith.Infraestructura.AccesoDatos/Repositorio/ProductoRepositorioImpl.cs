01885b9f5389a2d1e5fc3c573e4995867511b14a
Juan Diego <102696740+juandiego252@users.noreply.github.com>
2025-07-06 20:10:52 -0500

    feat: Se agrego ProductoRepositorio

---
+++ Carfaith.Infraestructura.AccesoDatos/Repositorio/ProductoRepositorioImpl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carfaith.Dominio.Modelo.Abstracciones;
+using Carfaith.Dominio.Modelo.Entidades;
+using Carfaith.Infraestructura.AccesoDatos.EFCore;
+
+namespace Carfaith.Infraestructura.AccesoDatos.Repositorio
+{
+    public class ProductoRepositorioImpl : RepositorioImpl<Producto>, IProductoRepositorio
+    {
+        public ProductoRepositorioImpl(CarfaithDbContext context) : base(context)
+        {
+
+        }
+    }
+}
