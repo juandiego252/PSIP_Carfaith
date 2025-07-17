70af0dcabb2dd56834ca6d4439ddc229e21226e4
Juan Diego <102696740+juandiego252@users.noreply.github.com>
2025-07-17 10:05:23 -0500

    fix: LineaDeProducto Controller

--- Carfaith_API/Controllers/LineaDeProductoController.cs
+++ Carfaith_API/Controllers/LineaDeProductoController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Carfaith_API.Controllers
 {
     [ApiController]
-    [Route("api/lineaProductos")]
+    [Route("api/[controller]")]
     public class LineaDeProductoController : ControllerBase
     {
         private readonly ILineasDeProductoServicio _lineaProductoServicio;
