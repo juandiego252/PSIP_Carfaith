030dce710966ab9a4699a5252d5ee8a088485f92
BryanCabrera24 <147127942+dev-bryancabrera@users.noreply.github.com>
2025-07-27 14:50:38 -0500

    Agregar nuevos archivos de DetalleOrdenEgreso y migraciones

---
+++ Carfaith.Aplicacion.DTO/DTOs/DetalleOrdenEgreso/OrdenEgresoDetallesDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carfaith.Aplicacion.DTO.DTOs.DetalleOrdenEgreso
+{
+    public class OrdenEgresoDetallesDTO
+    {
+        public int? IdProductoProveedor { get; set; }
+
+        public int? Cantidad { get; set; }
+
+        public string? TipoEgreso { get; set; }
+
+        public int? UbicacionId { get; set; }
+    }
+}
---
+++ Carfaith.Infraestructura.AccesoDatos/Migrations/20250727063151_AlterarTablaOrdenEgresoyDetalles.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Carfaith.Infraestructura.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterarTablaOrdenEgresoyDetalles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tipo_egreso",
+                table: "Orden_egreso");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TipoEgreso",
+                table: "Detalle_orden_egreso",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TipoEgreso",
+                table: "Detalle_orden_egreso");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tipo_egreso",
+                table: "Orden_egreso",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: true);
+        }
+    }
+}
